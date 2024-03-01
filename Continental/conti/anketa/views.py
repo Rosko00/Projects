e77bea7c15c55f239655bacbebef167449102b01
Marián Gálik <marian.galik@gmail.com>
2024-03-01 17:30:49 +0100

    hmm

--- Continental/conti/anketa/views.py
+++ Continental/conti/anketa/views.py
@@ -1,4 +1,5 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse ("Toto je moja prvá aplikácia")
+    return HttpResponse("Ahoj Continental!")
